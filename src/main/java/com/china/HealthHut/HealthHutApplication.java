39257e73a1dfa6458d9a144593802ee3d26618a7
pc <pc@DESKTOP-6AQEV2O>
2019-06-17 09:47:26 +0800

    第一次提交

---
+++ src/main/java/com/china/HealthHut/HealthHutApplication.java
@@ -0,0 +1,13 @@
+package com.china.HealthHut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthHutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/china/HealthHut/HealthHutApplicationTests.java
@@ -0,0 +1,16 @@
+package com.china.HealthHut;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HealthHutApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
